a651ce85bfa1c2048a604c83e4a97c998e56be9b
2a-Ron <ematrix_frag@hotmail.com>
2019-03-02 21:26:01 -0800

    initial setup continued

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ var pictureOne = document.getElementById('pic1');
 var pictureTwo = document.getElementById('pic2');
 var pictureThree = document.getElementById('pic3');
 //constructor
-function displayImage (name, ext) {
-    this.name = name;
-    this.ext = ext;
-    this.filepath = `img/${name}.${ext}`;
-    allImages.push(this);
+function constructImage (name, ext) {
+  this.name = name;
+  this.ext = ext;
+  this.filepath = `img/${name}.${ext}`;
+  allImages.push(this);
 }
 
 new imageGallery('bag', 'jpg');
@@ -32,3 +32,4 @@ new imageGallery('unicorn', 'jpg');
 new imageGallery('usb', 'gif');
 new imageGallery('water-can', 'jpg');
 new imageGallery('wine-glass', 'jpg');
+
