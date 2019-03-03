9dfa9575de5ff3e82c3ee494bb98294f48488629
2a-Ron <ematrix_frag@hotmail.com>
2019-03-02 18:53:34 -0800

    working on initial setup

---
+++ js/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var allImages = [];
+var pictureOne = document.getElementById('pic1');
+var pictureTwo = document.getElementById('pic2');
+var pictureThree = document.getElementById('pic3');
+//constructor
+function displayImage (name, ext) {
+    this.name = name;
+    this.ext = ext;
+    this.filepath = `img/${name}.${ext}`;
+    allImages.push(this);
+}
+
+new imageGallery('bag', 'jpg');
+new imageGallery('banana', 'jpg');
+new imageGallery('bathroom', 'jpg');
+new imageGallery('boots', 'jpg');
+new imageGallery('breakfast', 'jpg');
+new imageGallery('bubblegum', 'jpg');
+new imageGallery('chair', 'jpg');
+new imageGallery('cthulhu', 'jpg');
+new imageGallery('dog-duck', 'jpg');
+new imageGallery('dragon', 'jpg');
+new imageGallery('pen', 'jpg');
+new imageGallery('pet-sweep', 'jpg');
+new imageGallery('scissors', 'jpg');
+new imageGallery('shark', 'jpg');
+new imageGallery('sweep', 'png');
+new imageGallery('tauntaun', 'jpg');
+new imageGallery('unicorn', 'jpg');
+new imageGallery('usb', 'gif');
+new imageGallery('water-can', 'jpg');
+new imageGallery('wine-glass', 'jpg');
