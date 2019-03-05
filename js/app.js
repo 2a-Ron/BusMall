4ce5b564f7651e4bca29ebd254653891f24cbef7
2a-Ron <ematrix_frag@hotmail.com>
2019-03-04 21:57:48 -0800

    3 random images display but still no duplicate blocker

--- js/app.js
+++ js/app.js
@@ -1,35 +1,73 @@
 'use strict';
-
-var allImages = [];
 var pictureOne = document.getElementById('pic1');
 var pictureTwo = document.getElementById('pic2');
 var pictureThree = document.getElementById('pic3');
+var allImages = [];
+var turnCounter = 0;
 //constructor
-function constructImage (name, ext) {
+function ImageGallery (name, ext, title) {
+  this.title = title;
   this.name = name;
   this.ext = ext;
   this.filepath = `img/${name}.${ext}`;
+  this.views = 0;
+  this.clicks = 0;
+  this.viewPercentage = 0;
+  this.clickPercentage = 0;
   allImages.push(this);
 }
 
-new imageGallery('bag', 'jpg');
-new imageGallery('banana', 'jpg');
-new imageGallery('bathroom', 'jpg');
-new imageGallery('boots', 'jpg');
-new imageGallery('breakfast', 'jpg');
-new imageGallery('bubblegum', 'jpg');
-new imageGallery('chair', 'jpg');
-new imageGallery('cthulhu', 'jpg');
-new imageGallery('dog-duck', 'jpg');
-new imageGallery('dragon', 'jpg');
-new imageGallery('pen', 'jpg');
-new imageGallery('pet-sweep', 'jpg');
-new imageGallery('scissors', 'jpg');
-new imageGallery('shark', 'jpg');
-new imageGallery('sweep', 'png');
-new imageGallery('tauntaun', 'jpg');
-new imageGallery('unicorn', 'jpg');
-new imageGallery('usb', 'gif');
-new imageGallery('water-can', 'jpg');
-new imageGallery('wine-glass', 'jpg');
+new ImageGallery('bag', 'jpg', 'imgOne');
+new ImageGallery('banana', 'jpg', 'imgTwo');
+new ImageGallery('bathroom', 'jpg', 'imgThree');
+new ImageGallery('boots', 'jpg', 'imgFour');
+new ImageGallery('breakfast', 'jpg', 'imgFive');
+new ImageGallery('bubblegum', 'jpg', 'imgSix');
+new ImageGallery('chair', 'jpg', 'imgSeven');
+new ImageGallery('cthulhu', 'jpg', 'imgEight');
+new ImageGallery('dog-duck', 'jpg', 'imgNine');
+new ImageGallery('dragon', 'jpg', 'imgTen');
+new ImageGallery('pen', 'jpg', 'imgEleven');
+new ImageGallery('pet-sweep', 'jpg', 'imgTwelve');
+new ImageGallery('scissors', 'jpg', 'imgThirteen');
+new ImageGallery('shark', 'jpg', 'imgFourteen');
+new ImageGallery('sweep', 'png', 'imgFifteen');
+new ImageGallery('tauntaun', 'jpg', 'imgSixteen');
+new ImageGallery('unicorn', 'jpg', 'imgSeventeen');
+new ImageGallery('usb', 'gif', 'imgEightteen');
+new ImageGallery('water-can', 'jpg', 'imgNineteen');
+new ImageGallery('wine-glass', 'jpg', 'imgTwenty');
+
+function generateGallery() {
+  var random1 = Math.floor(Math.random() * allImages.length);
+  var random2 = Math.floor(Math.random() * allImages.length);
+  var random3 = Math.floor(Math.random() * allImages.length);
+
+  pictureOne.src = allImages[random1].filepath;
+  pictureTwo.src = allImages[random2].filepath;
+  pictureThree.src = allImages[random3].filepath;
+  pictureOne.title = allImages[random1].title;
+  pictureTwo.title = allImages[random2].title;
+  pictureThree.title = allImages[random3].title;
+  allImages[random1].views++;
+  allImages[random2].views++;
+  allImages[random3].views++;
+/*
+  console.log('current img, ', allImages[random1]);
+  console.log('current img, ', allImages[random2]);
+  console.log('current img, ', allImages[random3]);
+  */
+}
+
+generateGallery();
+
+pictureOne.addEventListener('click', handleClick);
+pictureTwo.addEventListener('click', handleClick);
+pictureThree.addEventListener('click', handleClick);
+function handleClick(event) {
+  console.log('title, ', event.target.title);
+  turnCounter++;
+  generateGallery();
+  console.log('Turn Count: ', turnCounter);
+}
 
